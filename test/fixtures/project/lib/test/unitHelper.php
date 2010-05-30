9060b4f2c61d47ffdcfb9a688c6ab070adbbe124
Ryan Weaver <ryan@thatsquality.com>
2010-05-30 18:24:11 -0500

    Refactoring unit helper functions into an external file.

---
+++ test/fixtures/project/lib/test/unitHelper.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Utility class for helper unit test functions
+ */
+
+$menuItemHelper = dirname(__FILE__).'/../../plugins/ioMenuPlugin/test/fixtures/project/lib/test/unitHelper.php';
+if (!file_exists($menuItemHelper))
+{
+  throw new Exception('Cannot find ioMenuPlugin unit helper. Are submodules updated?');
+}
+require_once ($menuItemHelper);
+
+
+// clears the menu database and then creates a test tree
+function create_doctrine_test_tree(lime_test $t)
+{
+  $t->info('### Creating test tree.');
+
+  // create the root
+  $rt = create_root('rt');
+
+  $pt1 = new ioDoctrineMenuItem();
+  $pt1->name = 'pt1';
+  $pt1->save();
+  $pt1->getNode()->insertAsLastChildOf($rt);
+  $rt->refresh();
+
+  $pt2 = new ioDoctrineMenuItem();
+  $pt2->name = 'pt2';
+  $pt2->save();
+  $pt2->getNode()->insertAsLastChildOf($rt);
+  $rt->refresh();
+
+  $ch1 = new ioDoctrineMenuItem();
+  $ch1->name = 'ch11';
+  $ch1->save();
+  $ch1->getNode()->insertAsLastChildOf($pt1);
+  $pt1->refresh();
+
+  $rt->refresh();
+  $pt1->refresh();
+  $pt2->refresh();
+  $ch1->refresh();
+
+  return array(
+    'rt' => $rt,
+    'pt1' => $pt1,
+    'pt2' => $pt2,
+    'pt3' => $pt3,
+  );
+}
+
+// prints the test doctrine tree to the view
+function print_test_doctrine_tree(lime_test $t)
+{
+  $t->info('      Menu Structure   ');
+  $t->info('               rt      ');
+  $t->info('             /    \    ');
+  $t->info('          pt1      pt2 ');
+  $t->info('           |           ');
+  $t->info('          ch1          ');
+}
+
+// creates a root menu entry and optionally clears all of the menu records
+function create_root($name, $clearData = true)
+{
+  if ($clearData)
+  {
+    Doctrine_Core::getTable('ioDoctrineMenuItem')->createQuery()->delete()->execute();
+  }
+
+  $rt = new ioDoctrineMenuItem();
+  $rt->name = 'rt';
+  $rt->save();
+  Doctrine_Core::getTable('ioDoctrineMenuItem')->getTree()->createRoot($rt);
+
+  return $rt;
+}
--- test/unit/model/doctrine/PluginioDoctrineMenuItemTest.php
+++ test/unit/model/doctrine/PluginioDoctrineMenuItemTest.php
@@ -2,11 +2,12 @@
 
 require_once dirname(__FILE__).'/../../../bootstrap/functional.php';
 require_once $_SERVER['SYMFONY'].'/vendor/lime/lime.php';
+require_once sfConfig::get('sf_lib_dir').'/test/unitHelper.php';
 
 $t = new lime_test(5);
 
 $t->info('1 - Test getChildrenIndexedByName().');
-  extract(create_test_tree($t)); // create the tree and make its vars accessible
+  extract(create_doctrine_test_tree($t)); // create the tree and make its vars accessible
   print_test_tree($t);
 
   $children = $rt->getChildrenIndexedByName();
@@ -17,54 +18,8 @@ $t->info('1 - Test getChildrenIndexedByName().');
   $t->is(count($pt1->getChildrenIndexedByName()), 1, '->getChildrenIndexedByName() returns 1 item for pt1.');
   $t->is(count($pt2->getChildrenIndexedByName()), 0, '->getChildrenIndexedByName() returns 0 items for pt2.');
 
-// clears the menu database and then creates a test tree
-function create_test_tree(lime_test $t)
-{
-  $t->info('### Creating test tree.');
+$t->info('2 - Test persistFromMenuArray() in a varierty of situations.');
   Doctrine_Query::create()->from('ioDoctrineMenuItem')->delete()->execute();
-  $tbl = Doctrine_Core::getTable('ioDoctrineMenuItem');
+  
 
-  $rt = new ioDoctrineMenuItem();
-  $rt->name = 'rt';
-  $rt->save();
-  $tbl->getTree()->createRoot($rt);
 
-  $pt1 = new ioDoctrineMenuItem();
-  $pt1->name = 'pt1';
-  $pt1->save();
-  $pt1->getNode()->insertAsLastChildOf($rt);
-  $rt->refresh();
-
-  $pt2 = new ioDoctrineMenuItem();
-  $pt2->name = 'pt2';
-  $pt2->save();
-  $pt2->getNode()->insertAsLastChildOf($rt);
-  $rt->refresh();
-
-  $ch1 = new ioDoctrineMenuItem();
-  $ch1->name = 'ch11';
-  $ch1->save();
-  $ch1->getNode()->insertAsLastChildOf($pt1);
-  $pt1->refresh();
-
-  $rt->refresh();
-  $pt1->refresh();
-  $pt2->refresh();
-  $ch1->refresh();
-
-  return array(
-    'rt' => $rt,
-    'pt1' => $pt1,
-    'pt2' => $pt2,
-    'pt3' => $pt3,
-  );
-}
-function print_test_tree(lime_test $t)
-{
-  $t->info('      Menu Structure   ');
-  $t->info('               rt      ');
-  $t->info('             /    \    ');
-  $t->info('          pt1      pt2 ');
-  $t->info('           |           ');
-  $t->info('          ch1          ');
-}
