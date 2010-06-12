612070a4c45f746696b5f2a0e51bc293b807665c
Ryan Weaver <ryan@thatsquality.com>
2010-06-12 11:40:27 -0500

    [admin] Making the reorder fallback gracefully if a root menu item has no children to reorder.

--- modules/io_doctrine_menu/templates/_reorderList.php
+++ modules/io_doctrine_menu/templates/_reorderList.php
@@ -1,5 +1,9 @@
-<div class="sf_admin_list">
-  <div id="nested-set"></div>
-</div>
+<?php if ($menu->getNode()->getChildren()): ?>
+  <div class="sf_admin_list">
+    <div id="nested-set"></div>
+  </div>
 
-<?php include_partial('io_doctrine_menu/javascripts.js', array('menu' => $menu)) ?>
+  <?php include_partial('io_doctrine_menu/javascripts.js', array('menu' => $menu)) ?>
+<?php else: ?>
+  <h2>This root menu item has no children to reorder.</h2>
+<?php endif; ?>
