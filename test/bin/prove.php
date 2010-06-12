cfb11184515ed1ac5d59fb4d121bece9c16f238f
Ryan Weaver <ryan@thatsquality.com>
2010-06-11 22:15:21 -0500

    [test] Preventing the prove file from picking up other plugin tests.

--- test/bin/prove.php
+++ test/bin/prove.php
@@ -3,6 +3,6 @@
 include dirname(__FILE__).'/../bootstrap/unit.php';
 
 $h = new lime_harness(new lime_output_color());
-$h->register(sfFinder::type('file')->name('*Test.php')->in(dirname(__FILE__).'/..'));
+$h->register(sfFinder::type('file')->maxDepth(5)->name('*Test.php')->in(dirname(__FILE__).'/..'));
 
 exit($h->run() ? 0 : 1);
