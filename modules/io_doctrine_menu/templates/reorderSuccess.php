cc9c928e1e0ee27486c646bd88ba39a7201a6fb9
Ryan Weaver <ryan@thatsquality.com>
2010-06-12 11:43:02 -0500

    [admin] Link language change, minor.

--- modules/io_doctrine_menu/templates/reorderSuccess.php
+++ modules/io_doctrine_menu/templates/reorderSuccess.php
@@ -5,7 +5,7 @@
   <div id="sf_admin_content">
     <div id="sf_admin_actions_container">
       <ul class="sf_admin_actions">
-        <li class="sf_admin_action_new"><?php echo link_to('New', 'io_doctrine_menu_new') ?></li>
+        <li class="sf_admin_action_new"><?php echo link_to('Create a new menu', 'io_doctrine_menu_new') ?></li>
       </ul>
 
       <?php include_partial('io_doctrine_menu/reorderList', array('menu' => $menu)) ?>
